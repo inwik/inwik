e03e2f9f6cda570b478685f16805046ad01ab6ab
albertoi <albertoi@sirym.com>
2015-08-06 20:46:15 +0200

    Mejorado estilo de .proposal_card

--- app/views/proposals/proposal_card.php
+++ app/views/proposals/proposal_card.php
@@ -1,5 +1,12 @@
+<style>
+    article.proposal_card{
+       padding:25px;
+    }
+</style>
+
+
 <article class="proposal_card">
     <div class="link-proposal"><a href="/<?=__('propuestas')?>/<?=$data['prop_url']?>"><?=$data['prop_title']?></a></div><div class="date-proposal"><?=$data['prop_date']?></div>
     <p class="description-proposal"><?=$data['prop_description']?></p>
-    <p class="action-proposal"><a href="propuestas/propuesta_de_ejemplo1&amp;action=subscribe"><span class="action-icon" id="subscribe"></span>Subscribirse</a><a href="propuestas/propuesta_de_ejemplo1&amp;action=share"><span class="action-icon" id="share"></span>Recomendar</a><a href="propuestas/propuesta_de_ejemplo1"><span class="action-icon" id="read"></span>Leer más</a></p>
+    <p class="action-proposal"><a href="/<?=__('propuestas')?>/<?=$data['prop_url']?>?action=subscribe"><i class="fa fa-star"></i>Subscribirse</a><a href="/<?=__('propuestas')?>/<?=$data['prop_url']?>?action=share"><i class="fa fa-share-square-o"></i>Compartir</a><a href="/<?=__('propuestas')?>/<?=$data['prop_url']?>"><i class="fa fa-newspaper-o"></i>Leer más</a></p>
 </article>
